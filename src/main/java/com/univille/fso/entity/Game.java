8ee87985da47f6973e0d920b50b654c5829b8f82
guarannius-estudos <gusta.antonius@gmail.com>
2025-04-30 19:49:10 -0300

    Criando projeto.

---
+++ src/main/java/com/univille/fso/entity/Game.java
@@ -0,0 +1,49 @@
+package com.univille.fso.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+public class Game {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "id_developer")
+    private Developer developer;
+
+    @ManyToOne
+    @JoinColumn(name = "id_genre")
+    private Genre genre;
+
+    @ManyToOne
+    @JoinColumn(name = "id_age_range")
+    private AgeRange ageRange;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate releaseDate;
+
+    private double value;
+
+    private String image;
+}
